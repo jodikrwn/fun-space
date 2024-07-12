413bb31d5a07d809a4ab391299bf37a6c210b6f5
jodikrwn <jodikurni4wan@gmail.com>
2024-07-12 22:17:56 +0700

    Menambahkan Hitung Luas Persegi Panjang

--- app.py
+++ app.py
@@ -4,5 +4,14 @@
 print(
     "Hello World!\n",
     "My Name Is: Jodi Kurniawan\n",
-    "With NIM: 3312001105"
+    "With NIM: 3312001105\n"
 )
+
+# hitung luas persegi panjang
+def luas_persegi_panjang():
+    p = int(input('Masukan panjang persegi: '))
+    l = int(input('Masukan lebar persegi: '))
+    luas = p * l
+    print('Luas persegi adalah: ', luas)
+
+luas_persegi_panjang()
