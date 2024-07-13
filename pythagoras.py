60822159d38f2c47d199b3d4ac08ff6262deb5a4
paklong01 <paklong0011@gmail.com>
2024-07-14 00:26:57 +0700

    Uji Coba Hitung Pythagoras

---
+++ pythagoras.py
@@ -0,0 +1,8 @@
+# menghitung teorema pythagoras
+def main():
+    a = float(input('Masukan nilai a: '))
+    b = float(input('Masukan nilai b: '))
+    c = (a**2 + b**2)**0.5
+    print('Nilai c adalah', c)
+
+main()
